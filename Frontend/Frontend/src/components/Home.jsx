f11f10f473eab5572be68e3583b95a88d6de13fb
kummari-shivani23 <shivanikummari05@gmail.com>
2025-09-18 01:44:40 +0530

    just added hero

---
+++ Frontend/Frontend/src/components/Home.jsx
@@ -0,0 +1,23 @@
+import styles from './Home.module.css'
+import hero from '../assets/herobluetheme.jpg'
+function Home(){
+   return(
+       <div>
+          <main>
+            <nav>
+                <img src="" alt="logo" />
+                <button className={styles.signup}>Signup</button>
+            </nav>
+            <div  className={styles.hero} style={{backgroundImage:`url(${hero})`}}>
+              {/* <img src={hero} alt="" /> */}
+                   <div className={styles.motto}>Opportunites don't happen.You create them</div>
+           </div>
+          </main>
+        <section>
+                <h2>Current Jobs</h2>
+                
+         </section>
+       </div>
+   )
+}
+export default Home;
